f187f0e62adf9777b521d1c68df2bccf844aec1e
hung <61528065+hungtg7@users.noreply.github.com>
2025-11-10 10:26:52 +0700

    Modify key mappings and add git blame
    Updated key mappings for Hop plugin and added configuration for git-blame plugin.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -20,8 +20,8 @@ return {
     -- CHANGED: Added `keys` so Hop only loads when you press a key to use it.
     -- You will need to set your own keymaps to trigger it. This is an example.
     keys = {
-      { "s", "<cmd>HopChar1<cr>", mode = { "n", "v" }, desc = "Hop to character" },
-      { "S", "<cmd>HopWord<cr>", mode = { "n", "v" }, desc = "Hop to word" },
+      { "f", "<cmd>HopChar1<cr>", mode = { "n", "v" }, desc = "Hop to character" },
+      { "F", "<cmd>HopWord<cr>", mode = { "n", "v" }, desc = "Hop to word" },
     },
     config = function()
       require("hop").setup({})
@@ -116,5 +116,22 @@ return {
     config = function()
       require("avante").setup({})
     end,
+  },
+  {
+    "f-person/git-blame.nvim",
+    -- load the plugin at startup
+    event = "VeryLazy",
+    -- Because of the keys part, you will be lazy loading this plugin.
+    -- The plugin will only load once one of the keys is used.
+    -- If you want to load the plugin at startup, add something like event = "VeryLazy",
+    -- or lazy = false. One of both options will work.
+    opts = {
+        -- your configuration comes here
+        -- for example
+        enabled = true,  -- if you want to enable the plugin
+        message_template = " <date> • <author> •  <summary> • <<sha>>", -- template for the blame message, check the Message template section for more options
+        date_format = "%m-%d-%Y %H:%M:%S", -- template for the date, check Date format section for more options
+        virtual_text_column = 1,  -- virtual text start column, check Start virtual text at column section for more options
+    },
   },
 }
